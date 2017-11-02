bb8e6babab0bf19dca823a97888788722f81aa52
Ryusuke Yamaji <ryuryu.ymj@gmail.com>
2017-11-02 21:11:25 +0900

    リファクタリング

--- src/chemicalReaction/Play.java
+++ src/chemicalReaction/Play.java
@@ -2,9 +2,7 @@ package chemicalReaction;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.Sound;
 
 /**
  * プレイ画面の更新,描画を行うクラス.
